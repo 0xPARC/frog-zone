ecf6b93058fb0132b410ad680c44973d9c8a52ef
Marianne Borowiak <borowiak.mar@gmail.com>
2024-10-15 15:51:56 -0400

    add login with qr code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,12 @@
 import { PlayerInfo } from "./game/components/PlayerInfo";
+import { Login } from "./game/components/Login";
 import { getPlayerId } from "./utils/getPlayerId";
 
 function App() {
 	const playerId = getPlayerId();
 	return (
 		<div>
+			<Login />
 			<PlayerInfo playerId={Number(playerId)} />
 		</div>
 	);
---
+++ packages/client/src/const/env.const.ts
@@ -0,0 +1,2 @@
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+export const LOGIN_SERVER_URL = import.meta.env.VITE_LOGIN_SERVER_URL;
