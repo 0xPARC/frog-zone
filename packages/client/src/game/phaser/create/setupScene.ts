ffc8a0bda7bba460600a48409a6eedaebd2abaa3
Marianne Borowiak <borowiak.mar@gmail.com>
2024-10-16 08:54:06 -0400

    center on the active player

--- packages/client/src/game/phaser/create/setupScene.ts
+++ packages/client/src/game/phaser/create/setupScene.ts
@@ -5,14 +5,19 @@ import {
 } from "@smallbraingames/small-phaser";
 import createTilemap from "./createTilemap";
 import config from "./phaserConfig";
+import { PLAYER_CONFIG } from "../../../../player.config";
+import { getPlayerId } from "../../../utils/getPlayerId";
 
 const tileHeight = config.tilemap.tileHeight;
 const setupMainScene = async (scene: Phaser.Scene) => {
+	const playerId = Number(getPlayerId());
 	await getSceneLoadPromise(scene);
 
 	const tilemap = createTilemap(scene);
 	const camera = createCamera(scene.cameras.main, 1, 1, 1, 1, 2);
-	camera.centerOn(tileHeight, tileHeight);
+	const x = PLAYER_CONFIG[playerId].x || 1;
+	const y = PLAYER_CONFIG[playerId].y || 1;
+	camera.centerOn(tileHeight * x, tileHeight * y);
 	const input = createInput(scene.input);
 
 	return {
