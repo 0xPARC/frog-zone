61f5582d1d8e695e04fbf0504b78a91ba213791f
Marianne Borowiak <borowiak.mar@gmail.com>
2024-10-22 11:51:40 -0400

    add animation when game begins

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -6,14 +6,28 @@ import WinGameButton from "./game/components/WinGameButton";
 import useStore from "./game/store";
 import { GameFinishedOverlay } from "./game/components/GameFinishedOverlay";
 import { WaitingForPlayersOverlay } from "./game/components/WaitingForPlayersOverlay";
+import { useReward } from "react-rewards";
+import { useEffect } from "react";
 
 const MIN_PLAYERS = 4;
 const MIN_PLAYERS_TO_FORCE_START = 1;
 function App() {
+	const { reward: confettiReward, isAnimating: isConfettiAnimating } =
+		useReward("confettiReward", "confetti", {
+			elementCount: 100,
+			elementSize: 12,
+			spread: 200,
+			angle: -90,
+		});
 	const playerId = getPlayerId();
 	const gameId = useStore((state) => state.game?.gameId);
 	const gameStatus = useStore((state) => state.game?.status);
 	const gameMachines = useStore((state) => state.game?.machines);
+	useEffect(() => {
+		if (gameStatus === "ongoing") {
+			confettiReward();
+		}
+	}, [gameStatus]);
 	return (
 		<div>
 			<Login />
@@ -30,6 +44,7 @@ function App() {
 				{gameId && gameStatus === "ongoing" && (
 					<WinGameButton gameId={gameId} />
 				)}
+				{/* <button onClick={() => confettiReward()}>Confetti</button> */}
 			</div>
 			{gameId &&
 				gameStatus === "waiting_for_players" &&
@@ -44,6 +59,10 @@ function App() {
 					/>
 				)}
 			{gameId && gameStatus === "completed" && <GameFinishedOverlay />}
+			<div
+				id="confettiReward"
+				style={{ position: "absolute", top: "-60px", left: "50%" }}
+			/>
 		</div>
 	);
 }
