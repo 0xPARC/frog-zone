d679fd92a600c8b1ac62fb33b4b64ef78c90ae03
Marianne Borowiak <borowiak.mar@gmail.com>
2024-10-22 10:40:39 -0400

    configure login api from env variables

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,17 +1,19 @@
-import { LOGIN_SERVER_URL } from "./src/const/env.const";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: LOGIN_SERVER_URL,
-				changeOrigin: true,
-				secure: false,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_LOGIN_SERVER_URL,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
+	};
 });
