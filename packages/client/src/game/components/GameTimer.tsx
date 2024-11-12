7d8fba344f9ea5ec5e97c29136b6c2ecd6d73e8a
Marianne Borowiak <borowiak.mar@gmail.com>
2024-11-12 10:46:21 +0700

    jank fix: minimize re-renders for the game timer

--- packages/client/src/game/components/GameTimer.tsx
+++ packages/client/src/game/components/GameTimer.tsx
@@ -1,49 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { updateGameStatus } from "../../utils/updateGameStatus";
 import { updatePlayer } from "../../utils/updatePlayer";
 import useStore from "../store";
 
 const TIME_UNTIL_AUTO_END = 300 * 1000;
 
-export const GameTimer = ({playerId}: {playerId: number}) => {
-  const gameId = useStore.getState().game?.gameId as string;
-  const [countdown, setCountdown] = useState(TIME_UNTIL_AUTO_END / 1000);
-  const publicKey = useStore.getState().publicKey as string;
-  const player = useStore((state) => state.getPlayerById(playerId));
+export const GameTimer = ({ playerId }: { playerId: number }) => {
+	const gameId = useStore.getState().game?.gameId as string;
+	const publicKey = useStore.getState().publicKey as string;
+	const player = useStore((state) => state.getPlayerById(playerId));
 
-  useEffect(() => {
-    const handleGameEnd = async () => {
-      await updatePlayer({
-  			score: player?.points || 0,
-  			publicKey,
-  			gameId,
-  		});
-  		await updateGameStatus({
-  			gameId,
-  			status: "completed",
-  		});
-  	};
+	const [countdown, setCountdown] = useState(TIME_UNTIL_AUTO_END / 1000);
+	const countdownRef = useRef(countdown);
+	countdownRef.current = countdown;
 
-		const timer = setInterval(async () => {
-		  updatePlayer({
-  			score: player?.points || 0,
-  			publicKey,
-  			gameId,
-  		});
+	const handleGameEnd = useCallback(async () => {
+		await updatePlayer({
+			score: player?.points || 0,
+			publicKey,
+			gameId,
+		});
+		await updateGameStatus({
+			gameId,
+			status: "completed",
+		});
+	}, [gameId, player?.points, publicKey]);
+
+	useEffect(() => {
+		const updatePlayerScore = () => {
+			if (countdownRef.current % 5 === 0) {
+				updatePlayer({
+					score: player?.points || 0,
+					publicKey,
+					gameId,
+				});
+			}
+		};
+
+		const timer = setInterval(() => {
 			setCountdown((prevCountdown) => {
 				if (prevCountdown <= 1) {
 					clearInterval(timer);
 					handleGameEnd();
 					return 0;
 				}
+				updatePlayerScore();
 				return prevCountdown - 1;
 			});
 		}, 1000);
 
 		return () => clearInterval(timer);
-	}, [gameId, player, publicKey]);
+	}, [gameId, handleGameEnd, player?.points, publicKey]);
 
-	const secondsRemaining = (countdown).toFixed(0);
+	const secondsRemaining = countdown.toFixed(0);
 
 	const containerStyle = {
 		...styles.container,
@@ -52,17 +61,16 @@ export const GameTimer = ({playerId}: {playerId: number}) => {
 	};
 
 	return (
-		<>
-			<div style={containerStyle}>
-				{countdown > 0 ? (
-					<p>
-						<b>{secondsRemaining}</b> seconds left to defeat the DRAGON. Go NORTH!
-					</p>
-				) : (
-					<p>GAME OVER</p>
-				)}
-			</div>
-		</>
+		<div style={containerStyle}>
+			{countdown > 0 ? (
+				<p>
+					<b>{secondsRemaining}</b> seconds left to defeat the DRAGON.
+					Go NORTH!
+				</p>
+			) : (
+				<p>GAME OVER</p>
+			)}
+		</div>
 	);
 };
 
